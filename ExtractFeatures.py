ee4b723cead83295301a5a12b1a9dd9d0845c311
stonedalpaca <stonedalpaca@gmail.com>
2013-11-22 13:05:01 -0600

    Update ExtractFeatures.py

--- ExtractFeatures.py
+++ ExtractFeatures.py
@@ -24,5 +24,13 @@ def parseText(text):
 text = "The mother loves the daughter.  Joan knows that Bob wants to go to the beach that is sandy.  She asks if he wants a ride."
 
 parseDict = parseText(text)
-
-print parseDict
+POS_Array = []
+Dep_Array = []
+for sentence in parseDict['sentences']:
+    for dep in sentence['dependencies']:
+	temp
+	if "nsubj" in dep[0]:
+	    
+    for word in sentence['words']:
+	POS_Array.append(word[1]['PartOfSpeech'])
+print POS_Array
